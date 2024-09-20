04aea67363baa66048f1933aedc78e5bde47d4e6
Varun_Shivaram <varunshivaram2000@gmail.com>
2024-09-20 21:12:15 +0530

    this is my first python file in github

---
+++ tip.py
@@ -0,0 +1,6 @@
+print("Hello Welcome!, I am Tip Calculator")
+a=int(input("Kindly enter your total bill amount\n"))
+b=int(input("How much would you give as tip \n10$\n12$\n15$\n"))
+c=int(input("How many of you are splitting the bill\n"))
+fnl=(a+b)/c
+print(f"So you would have to pay {fnl}$")
