08ef5de1fb415bac1260fe1483ebf9dd94d6470e
VarunShivaram <varunshivaram2000@gmail.com>
2024-10-03 20:42:02 +0530

    This code performs a blind auction and announces the higest bidded

---
+++ blindauction.py
@@ -0,0 +1,39 @@
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(logo)
+print("Hello Bidders! Excited to see you here get ready for the auction")
+a=True
+bid={}
+while(a==True):
+    print("\n"*100)
+    b=input("Please enter 'yes' if you want to bid else 'no':\n")
+    if(b=='yes'):
+        print("\n\n")
+        e=input("please enter your name:\n")
+        if(e in bid):
+            print("The person has already submitted their bid")
+            continue
+        c=int(input("Please enter your bidding amount:\n$"))
+        bid.update({e:c})
+    elif(b=='no'):
+        a=False
+    else:
+        print("Please enter a valid option")
+max = list(bid.keys())[0]
+for key in bid:
+    if(bid[key]>bid[max]):
+        max=key
+print("The Highest bidded among the participants is \n")
+print(f"{max} with a bidding amount of {bid[max]}$\n")
+print(f"Congratulations {max}!!!")
